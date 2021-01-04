fb741e8e8b47a79484b3cdded5313cd426c9c5cb
李敏 <limin@mobiletopgame.com>
2021-01-04 10:43:38 +0800

    test maven release plugin

--- src/main/java/com/bf/demo/Application.java
+++ src/main/java/com/bf/demo/Application.java
@@ -1,4 +1,4 @@
-package com.bf;
+package com.bf.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/bf/demo/web/controller/DockerController.java
+++ src/main/java/com/bf/demo/web/controller/DockerController.java
@@ -1,4 +1,4 @@
-package com.bf.web.controller;
+package com.bf.demo.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
